390c205fba79d2fa2b15f246cb25b22a7d413d58
Dimi Chakarov <dimitar@dchakarov.com>
2024-12-04 23:26:31 +0000

    part 2

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -16,12 +16,33 @@ func main() {
     let line = lines.joined(separator: " ")
     
     let result = parseLine(line)
-    let final = result.reduce(0) { $0 + $1.0 * $1.1 }
-    print(result)
-    print(final)
+    let part1 = result.reduce(0) { $0 + $1.0 * $1.1 }
+    print(part1)
+    
+    let commands = parseString(line)
+
+    print(calcPart2(muls: result, commands: commands))
 }
 
-func parseLine(_ line: String) -> [(Int, Int)] {
+func calcPart2(muls: [(Int, Int, String.Index)], commands: [(String, String.Index)]) -> Int {
+    var result = 0
+    
+    for mul in muls {
+        let (a, b, index) = mul
+        
+        if let command = commands.last(where: { $0.1 < index }) {
+            if command.0 == "do()" {
+                result += a * b
+            }
+        } else {
+            result += a * b
+        }
+    }
+    
+    return result
+}
+
+func parseLine(_ line: String) -> [(Int, Int, String.Index)] {
     let digit1 = Reference(Int.self)
     let digit2 = Reference(Int.self)
     
@@ -37,14 +58,30 @@ func parseLine(_ line: String) -> [(Int, Int)] {
         ")"
     }
     
-    var response = [(Int, Int)]()
+    var response = [(Int, Int, String.Index)]()
     
     let result = line.matches(of: search)
     for match in result {
-        response.append((match[digit1], match[digit2]))
+        response.append((match[digit1], match[digit2], match.range.lowerBound))
     }
     
     return response
+}
+
+func parseString(_ line: String) -> [(String, String.Index)] {
+    let str = Reference(String.self)
+    let search = Regex {
+        TryCapture(as: str) {
+            ChoiceOf {
+                "do()"
+                "don't()"
+            }
+        } transform: { String($0) }
+    }
+        
+    let result = line.matches(of: search)
+    
+    return result.map { ($0[str], $0.range.lowerBound) }
 }
 
 main()
